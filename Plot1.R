9651ddf46789537fe7202be8e194ae0663a35523
Wen <wye0220@gmail.com>
2014-07-27 04:24:21 -0400

    Adding Plot1.R and png image

--- Plot1.R
+++ Plot1.R
@@ -1,6 +1,17 @@
 
-library(plyr)
-library(ggplot2)
+if (!require("plyr")) {
+    install.packages("plyr")
+}
 
-NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
+library(plyr)
+
+#Load data
+NEI <- readRDS("./exdata-data-NEI_data/summarySCC_PM25.rds")
+SCC <- readRDS("./exdata-data-NEI_data/Source_Classification_Code.rds")
+
+data <- with(NEI, aggregate(Emissions, by = list(year), sum))
+
+png("./figure/plog1.png")
+plot(data, type="l", ylab=expression("Total Emissions, PM"[2.5]), xlab="Year",main="Total Emissions in the United States from 1999 to 2008")
+
+dev.off()
